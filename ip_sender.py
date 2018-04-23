3050b383080bc80126db596d620680c956494b69
Craig Blackham <craigblackham@gmail.com>
2018-04-23 11:21:58 -0700

    added mac addr checking

--- ip_sender.py
+++ ip_sender.py
@@ -1,7 +1,14 @@
 import urllib
 
-my_ip = urllib.urlopen("http://ipinfo.io/ip").read()
-#print (site)
-result = urllib.urlopen("http://cogsix.com/ip_listener/" + my_ip)
- 
+my_ip = urllib.urlopen("http://ipinfo.io/ip").read().strip()
+#print (my_ip)
 
+#adding local mac address to prevent spam
+my_mac = open("/sys/class/net/wlan0/address").readline().strip()
+#print (my_mac)
+
+url = "http://cogsix.com/ip_listener/" + my_ip + "/" + my_mac
+print (url)
+
+result = urllib.urlopen(url)
+#print (result)
